7f918328692d0419ea7d9a6038b31677ffad929a
Alex Kovalenko <iamaleksandr@gmail.com>
2021-04-28 00:13:47 +0300

    mockup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Student} from './core/model/student';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'students-managment';
+
+  studentsData =
+    ' [{ "fname": "Tevon", "lname": "Barens", "grade": 43.0, "year": 1980 }, { "fname": "Josef", "lname": "Cohen", "grade": 60.0, "year": 1980 }, { "fname": "Nati", "lname": "Green", "grade": 99.0, "year": 2001 }, { "fname": "Shakil", "lname": "Jareh", "grade": 88.0, "year": 2001}]';
+  students: Student[];
+  selectedStudent: Student;
+
+  constructor() {
+    this.students = JSON.parse(this.studentsData);
+    this.selectedStudent = this.students[0];
+  }
 }

a866722b76a7a98036a114722f3c5af328b62527
Alex Kovalenko <akovalenko@callfire.com>
2021-04-28 00:32:46 +0300

    selects mockups

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
+import {NgSelectModule} from '@ng-select/ng-select';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, NgSelectModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })

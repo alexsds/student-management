e86a628a0d5967422896b989ffe8fc21b57da99f
Alex Kovalenko <iamaleksandr@gmail.com>
2021-04-28 00:32:46 +0300

    selects mockups

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,8 @@ export class AppComponent {
     ' [{ "fname": "Tevon", "lname": "Barens", "grade": 43.0, "year": 1980 }, { "fname": "Josef", "lname": "Cohen", "grade": 60.0, "year": 1980 }, { "fname": "Nati", "lname": "Green", "grade": 99.0, "year": 2001 }, { "fname": "Shakil", "lname": "Jareh", "grade": 88.0, "year": 2001}]';
   students: Student[];
   selectedStudent: Student;
+  years = [1940, 2012, 1960, 2009, 1980];
+  classes = ['Biology', 'Chemistry', 'ComputerScience'];
 
   constructor() {
     this.students = JSON.parse(this.studentsData);

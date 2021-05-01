6e67c8e98c55bcc4716409b4171b89f59b963ebe
Alex Kovalenko <iamaleksandr@gmail.com>
2021-05-01 12:32:33 +0300

    implement filters

--- src/app/store/students/students.effects.ts
+++ src/app/store/students/students.effects.ts
@@ -1,29 +1,77 @@
 import {Actions, createEffect, ofType} from '@ngrx/effects';
 import {Injectable} from '@angular/core';
-import {catchError, map, switchMap} from 'rxjs/operators';
+import {catchError, map, switchMap, withLatestFrom} from 'rxjs/operators';
 import {forkJoin, of} from 'rxjs';
 import {StudentsService} from '../../core/service/students.service';
 import * as StudentsActions from './students.actions';
+import {Student} from '../../core/model/student';
+import {Store} from '@ngrx/store';
+import * as fromApp from '../app.reducer';
+import {getSelectedClassType, getSelectedYear} from './students.selectors';
 
 @Injectable()
 export class StudentsEffects {
-  fetch$ = createEffect(() => {
+  fetchData$ = createEffect(() => {
     return this.actions$.pipe(
-      ofType(StudentsActions.FETCH),
+      ofType(StudentsActions.FETCH_DATA),
       switchMap(() => {
         return forkJoin({
           years: this.studentsService.years(),
-          classes: this.studentsService.classes(),
-          students: this.studentsService.get(),
+          classTypes: this.studentsService.classTypes(),
         }).pipe(
           map(
-            ({years, classes, students}) => new StudentsActions.FetchSuccess({years, classes, students}),
-            catchError(() => of(new StudentsActions.FetchFail()))
+            ({years, classTypes}) => new StudentsActions.FetchDataSuccess({years, classTypes}),
+            catchError(() => of(new StudentsActions.FetchDataFail()))
           )
         );
       })
     );
   });
 
-  constructor(private actions$: Actions, private studentsService: StudentsService) {}
+  selectClassType$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(StudentsActions.SELECT_CLASS_TYPE),
+      switchMap((action: StudentsActions.SelectClassType) => {
+        return this.studentsService.years(action.payload.classType).pipe(
+          map(
+            (years) => new StudentsActions.FetchYearsSuccess({years}),
+            catchError(() => of(new StudentsActions.FetchYearsFail()))
+          )
+        );
+      })
+    );
+  });
+
+  selectYear$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(StudentsActions.SELECT_YEAR),
+      switchMap((action: StudentsActions.SelectYear) => {
+        return this.studentsService.classTypes(action.payload.year).pipe(
+          map(
+            (classTypes) => new StudentsActions.FetchClassTypesSuccess({classTypes}),
+            catchError(() => of(new StudentsActions.FetchClassTypesFail()))
+          )
+        );
+      })
+    );
+  });
+
+  fetchStudents$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(StudentsActions.SELECT_YEAR, StudentsActions.SELECT_CLASS_TYPE),
+      withLatestFrom(this.store.select(getSelectedYear), this.store.select(getSelectedClassType)),
+      switchMap(([, selectedYear, selectedClassType]) => {
+        return this.studentsService.get({year: selectedYear, classType: selectedClassType}).pipe(
+          map((students: Student[]) => new StudentsActions.FetchStudentsSuccess({students})),
+          catchError(() => of(new StudentsActions.FetchStudentsFail()))
+        );
+      })
+    );
+  });
+
+  constructor(
+    private actions$: Actions,
+    private store: Store<fromApp.State>,
+    private studentsService: StudentsService
+  ) {}
 }
